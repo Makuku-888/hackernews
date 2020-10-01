ab73796a1d93949831f2db103a7cfd3ea5d7644f
Laura Ningel <laura.ningel@gmail.com>
2020-10-01 17:17:09 +0200

    implementation level 2

--- src/Components/MyList.js
+++ src/Components/MyList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const List = (props) => {
+    console.log("PROPS", props.content)
     return (
         <div>
         {props.query !== "blockchain" && !props.content.length && (
@@ -8,22 +9,19 @@ const List = (props) => {
             No items are matching your search! Please try another keyword.
             </div>
         )}
-        {props.error && <div>Something did not work out here...</div>}
+        {props.error && <div>Something did not work out here...</div>}        
         <ul>
-            {props.loading ? (
-            <div>Loading data...</div>
-            ) : (
-            props.content.map((el) => {
+            {props.content.filter(el => el.story_title || el.title).map((el) => {
                 return (
                 <li key={el.objectID}>
                     <span className="date">{el.created_at.slice(0, el.created_at.indexOf("T") )}</span>
-                    <a href={el.url} target="blank"><span className="title">{el.title}</span></a><br/>
+                    <a href={el.url} target="blank"><span className="title">{el.title || el.story_title}</span></a><br/>
                     <span className="author">by: {el.author}</span>
                     <span className="points">Points: {el.points}</span>
                 </li>
                 );
             })
-            )}
+            }
         </ul>
         </div>
     );
