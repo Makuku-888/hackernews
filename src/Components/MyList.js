5523b29b3548a22fc26fe2e8b8983931739e8fac
Laura Ningel <laura.ningel@gmail.com>
2020-10-01 16:06:07 +0200

    level 2

--- src/Components/MyList.js
+++ src/Components/MyList.js
@@ -3,18 +3,29 @@ import React from 'react';
 const List = (props) => {
     return (
         <div>
-        <ol className="listContainer">
-            {props.content.map(el =>{
-                return <li>
-                <span className="date">{el.created_at.slice(0, el.created_at.indexOf("T") )}</span>
-                <a href={el.url} target="blank"><span className="title">{el.title}</span></a><br/>
-                <span className="author">by: {el.author}</span>
-                <span className="points">Points: {el.points}</span>
+        {props.query !== "blockchain" && !props.content.length && (
+            <div>
+            No items are matching your search! Please try another keyword.
+            </div>
+        )}
+        {props.error && <div>Something did not work out here...</div>}
+        <ul>
+            {props.loading ? (
+            <div>Loading data...</div>
+            ) : (
+            props.content.map((el) => {
+                return (
+                <li key={el.objectID}>
+                    <span className="date">{el.created_at.slice(0, el.created_at.indexOf("T") )}</span>
+                    <a href={el.url} target="blank"><span className="title">{el.title}</span></a><br/>
+                    <span className="author">by: {el.author}</span>
+                    <span className="points">Points: {el.points}</span>
                 </li>
-            })}
-        </ol>
+                );
+            })
+            )}
+        </ul>
         </div>
-
     );
 }
 
--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,25 +1,27 @@
 import React from "react";
 
 const SearchBar = (props) => {
-
     const handleSubmit = (e) => {
-      e.preventDefault();
-      console.log("inital url", props.url);
-      const newUrl =
-        props.url.slice(0, props.url.indexOf("=") + 1) +
-        e.target.children[0].value + "&hitsPerPage=20";
-      props.seturl(newUrl);
-      console.log("url was reset with: ", newUrl);
+    e.preventDefault();
+    if (!e.target.children[0].value) {
+        alert("please enter some keyword!");
+        return;
+    }
+    console.log("inital query:", props.query);
+    const newQuery = e.target.children[0].value;
+    props.setquery(newQuery);
+    console.log("url was reset with:", newQuery);
+    e.target.children[0].value = "";
     };
-  
+
     return (
-        <div className="search-container">
-            <form className="search" onSubmit={handleSubmit}>
-                <input type="text"></input>
-                <button>search topic</button>
-            </form>
-      </div>
+    <div className="search-container">
+        <form onSubmit={handleSubmit}>
+            <input className="search" type="text"></input>
+            <button className="search">search topic</button>
+        </form>
+    </div>
     );
-  };
+};
 
 export default SearchBar;
